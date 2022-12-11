264a8c8f51ec419dbbf3c77296bc7a12d662f435
aryav123 <arya74331@gmail.com>
2022-12-11 20:17:19 +0530

    (driver commands)

--- src/test/java/seleniumcommands/BasicCommands.java
+++ src/test/java/seleniumcommands/BasicCommands.java
@@ -8,23 +8,29 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BasicCommands {
     public static void main(String[] args) {
         WebDriver driver;
-        /** Creating webdriver instance**/
-        driver = new ChromeDriver();
+        driver = new ChromeDriver();                    /** Creating webdriver instance**/
+        driver.manage().window().maximize();            /** Maximize window**/
+        driver.manage().deleteAllCookies();             /** Delete all cookies**/
         driver.get("https://selenium.obsqurazone.com/index.php");
-        driver.manage().window().maximize();
+        String currentURL = driver.getCurrentUrl();
+        System.out.println(currentURL);
+        String title=driver.getTitle();                 /** Get title**/
+        System.out.println(title);
+        String sourceCode=driver.getPageSource();       /** Get sourcecode**/
+        System.out.println(sourceCode);
         driver.close();
 
-        /**Launching Edge driver**/
-        driver = new EdgeDriver();
-        driver.get("https://selenium.obsqurazone.com/index.php");
-        driver.manage().window().maximize();
-        driver.close();
-
-        /**Launching Firefox driver**/
-        driver = new FirefoxDriver();
-        driver.get("https://selenium.obsqurazone.com/index.php");
-        driver.manage().window().maximize();
-        driver.close();
+//        /**Launching Edge driver**/
+//        driver = new EdgeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://selenium.obsqurazone.com/index.php");
+//        driver.close();
+//
+//        /**Launching Firefox driver**/
+//        driver = new FirefoxDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://selenium.obsqurazone.com/index.php");
+//        driver.close();
 
     }
 }
